ce7d4f518c7cb63b1223f5d21edc318e645c4793
justAnotherAverageCoder <justanother1982@proton.me>
2024-06-21 23:20:53 -0400

    Add files via upload

--- nShell.nim
+++ nShell.nim
@@ -3,20 +3,46 @@ import std/osproc
 import std/rdstdin
 import strutils
 import std/terminal
+import std/json
 
-proc sEcho(color = fgCyan, str: string) =
+
+
+
+
+
+
+
+proc sEcho(color: ForegroundColor, str: string) =
     stdout.styledWriteLine(color, styleBright, str)
 
 proc read(): string =
-    return readLineFromStdin(os.getCurrentDir() & "> ")
+    return readLineFromStdin(os.getCurrentDir() & ">")
 
 proc process(s: string): seq[string] =
     return rsplit(s, " ")
 
 
+proc getConf(): string =
+    if fileExists("nShell.conf"):
+        return readFile("nShell.conf")
+    else:
+        writeFile("nShell.conf", $parseJson("""{"color": "fgDefault"}"""))
+        return readFile("nShell.conf")
+
+proc stringToFg(str: string): ForegroundColor =
+    for x in ForegroundColor:
+        if x == parseEnum[ForegroundColor]($(replace($str, """"""", ""))):
+            return x
+        else:
+            discard
+    return fgRed
 
 
-proc exec(cmd: string) = 
+
+
+
+
+proc exec(cmd: string, color: ForegroundColor) = 
     var splitCmd = process(cmd)
     if splitCmd[0] == "cd":
         try:
@@ -26,16 +52,16 @@ proc exec(cmd: string) =
     elif cmd == "quit":
         quit()
     else:
-        var y = execProcess(cmd)
-        y.removeSuffix("\n")
-        sEcho(fgYellow, y)
+        var res = execProcess(cmd)
+        res.removeSuffix("\n")
+        sEcho(color, res)
 
 
 
 proc main() =
+    let configs = parseJson(getConf())
     while true:
-        var read = read()
-        exec(read)
+        exec(read(), stringToFg($configs["color"]))
 
 
 main()
