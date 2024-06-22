9cfd1d640dcb20e9d731f0290f3b6a96f3ebb128
justAnotherAverageCoder <justanother1982@proton.me>
2024-06-22 12:02:07 -0400

    Add files via upload
    added cd functionality

--- nShell.nim
+++ nShell.nim
@@ -1,6 +1,7 @@
 import nigui
 import osproc
-
+import std/os
+import std/strutils
 
 proc exe(cmd: string): string =
     return execProcess(cmd)
@@ -15,16 +16,30 @@ proc makeLabel(con: Container): Label =
     con.add(label)
     return label
 
+
+proc split(str: string): seq[string] =
+    return rsplit(str, " ")
+
 proc event(tex: TextBox, lab: Label) =
     tex.onKeyDown = proc(event: KeyboardEvent) =
         if $event.key == "Key_Return":
+            var spl = split(tex.text)
             lab.text = ""
             if tex.text == "clear":
                 lab.text = lab.text & ""
             elif tex.text == "quit":
                 quit()
+            elif spl[0] == "cd":
+                if spl[1] == "~":
+                    setCurrentDir(getHomeDir())
+                else:
+                    try:
+                        setCurrentDir(spl[1])
+                    except:
+                        lab.text = "NOT A DIR"
+
+
             else:
-                echo tex.text
                 lab.text = lab.text & exe(tex.text)
             tex.text = ""
 
@@ -43,6 +58,8 @@ proc setupWindow(): Window =
 
 proc setupContainer(win: Window): Container =
     var container = newLayoutContainer(Layout_Vertical)
+    container.padding = 50
+    container.frame = newFrame("nShell")
     win.add(container)
     return container
 
@@ -53,8 +70,8 @@ proc main() =
     var window = setupWindow()
     var container = setupContainer(window)
 
-    var input = makeInput(container)
     var label = makeLabel(container)
+    var input = makeInput(container)
     event(input, label)
     window.show()
     app.run()
